2f9a4adc8741aaff8731bb34f0128e33bfa91eb1
Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>
2026-02-28 10:41:17 +0300

    wip: testing
    Signed-off-by: Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>

--- build.sh
+++ build.sh
@@ -90,7 +90,7 @@ echo "COMPILER_STRING=$COMPILER_STRING" >> $GITHUB_ENV
 
 cd $KSRC
 
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s builtin
+curl -LSs "https://raw.githubusercontent.com/ahmed-alnassif/SukiSU-Ultra/builtin/kernel/setup.sh" | bash -s builtin
 
 SUSFS_DIR="$WORKDIR/susfs"
 SUSFS_PATCHES="${SUSFS_DIR}/kernel_patches"
